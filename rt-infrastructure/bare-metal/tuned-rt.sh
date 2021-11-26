257d3a0541eadb6116cad2e28acb33fd0a062c4f
Juan Pablo Noreña <jpnorenam@unal.edu.co>
2021-11-26 07:39:14 -0500

    wip infra

--- rt-infrastructure/bare-metal/tuned-rt.sh
+++
@@ -1,6 +0,0 @@
-# Use the tuned tool for improving general real-time performance
-dnf install tuned-profiles-realtime
-echo "realtime" > /etc/tuned/active_profile
-echo "isolated_cpucores=25-39" >> /etc/tuned/realtime-variables.conf
-systemctl enable tuned && systemctl start tuned
-systemctl reboot
--- rt-infrastructure/kvm/kvm-host.sh
+++ rt-infrastructure/kvm/kvm-host.sh
@@ -1,12 +1,46 @@
+#!/bin/bash
+
+set -e
+
+vagrant_cwd=${pwd}
+
 dnf -y update
 
-# Install virtualization packages
-dnf -y install bridge-utils libvirt virt-install qemu-kvm
-
-# Install vagrant and plugin for libvirt provider
+# Install vagrant
 dnf install -y dnf-plugins-core
 dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
-dnf -y install vagrant
+dnf install -y vagrant
+
+# Install libvirt packages
+dnf install -y gcc libvirt libvirt-devel libxml2-devel make ruby-devel libguestfs-tools qemu-kvm
+systemctl start libvirtd
+systemctl enable libvirtd
+
+# Build libssh
+mkdir /tmp/libssh
+dnf download --source libssh --destdir /tmp/libssh
+rpm2cpio /tmp/libssh/libssh-*.src.rpm | cpio -imdV -D /tmp/libssh
+tar xf /tmp/libssh/libssh-*.tar.xz -C /tmp/libssh
+mkdir /tmp/libssh/build && cd /tmp/libssh/build
+cmake ../libssh-* -DOPENSSL_ROOT_DIR=/opt/vagrant/embedded/
+make -j$(nproc)
+cp lib/libssh* /opt/vagrant/embedded/lib64
+rm -rf /tmp/libssh
+
+# Build krb5
+mkdir /tmp/krb5
+dnf download --source krb5-libs --destdir /tmp/krb5
+rpm2cpio /tmp/krb5/krb5-*.src.rpm | cpio -imdV -D /tmp/krb5
+tar xf /tmp/krb5/krb5-*.tar.gz -C /tmp/krb5
+cd /tmp/krb5/krb5-*/src
+./configure
+make -j$(nproc)
+cp -a lib/crypto/libk5crypto.* /opt/vagrant/embedded/lib64/
+rm -rf /tmp/krb5
+
+# Install libvirt provider
+cd $vagrant_cwd
+export CONFIGURE_ARGS="with-libvirt-include=/usr/include/libvirt with-libvirt-lib=/usr/lib64"
 vagrant plugin install vagrant-libvirt
 
 # Start the virtual machine
